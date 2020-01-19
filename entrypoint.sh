b47291c2e8f31590e3c676ae49a605f244a17129
Sandro Wyss <sandro@brudi.com>
2020-01-19 06:37:26 +0100

    fix: broken release repo check

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,9 +32,7 @@ if [ -z "$REPO_PATH" ]
 then
   # mesh convention: expect apps in 'apps' folder
   REPO_PATH="apps/${APP}"
-  
-  if [[! -d "$REPO_PATH" ] && [ "$is_fallback_app_name" = true ]]; then
-    # when using fallback app name but the 'apps' folder 
+  if [[ ! -d "$REPO_PATH" ]] && [ "$is_fallback_app_name" = true ] ; then    # when using fallback app name but the 'apps' folder 
     # does not exist, we fallback to the 'base' directory. 
     #
     # The app is now only used in the commit message. 
