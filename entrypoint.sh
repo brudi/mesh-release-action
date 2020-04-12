2a553da4417e5d057ca9d6ac18eb38fc361d91bd
Sandro Wyss <sandro@brudi.com>
2020-04-12 05:02:05 +0200

    refactor: change directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,102 +19,112 @@ MERGE=${13}
 commit_msg=$(git log -1 --pretty=%B)
 action_root=$(pwd)
 install_folder=$GITHUB_WORKSPACE/install
+kustom_folder=""
 
 is_fallback_app_name=false
-# default
+
+# default app name
 if [ -z "$APP" ]; then
   is_fallback_app_name=true
   APP=${PWD##*/}
   echo "app param not defined. using current folder name ${APP}"
 fi
 
-# clone and configure the catalog Git repository
-git clone "https://brudicloud:${TOKEN}@${REPO}" catalog
-cd catalog
-git checkout ${REF}
-git config --global user.email cloud@brudi.com
-git config --global user.name Mesh
-
-# configure path to app configuration
-if [ -z "$REPO_PATH" ]; then
-  # mesh convention: expect apps in 'apps' folder
-  REPO_PATH="apps/${APP}"
-
-  if [[ ! -d "$REPO_PATH" ]] && [ "$is_fallback_app_name" = true ]; then # when using fallback app name but the 'apps' folder
-    # does not exist, we fallback to the 'base' directory.
-    #
-    # The app is now only used in the commit message.
-    REPO_PATH='base'
-  fi
-fi
-
-echo "release Mesh app '$APP' ($VERSION) in $REPO"
-echo "-> $commit_msg"
-
-# create app config directory if it doesn't exist yet
-if [[ ! -d "$REPO_PATH" ]]; then
-  mkdir -p $REPO_PATH
-fi
-
-# change to desired app config directory
+# set desired app kustomization directory
 if [ ! -z "$OVERLAY" ]; then
   echo "editing overlay kustomization at overlays/$OVERLAY"
-  cd $install_folder/overlays/$OVERLAY
+  kustom_folder = $install_folder/overlays/$OVERLAY
 else
   echo "editing base kustomization"
-  cd $install_folder/base
+  kustom_folder = $install_folder/base
 fi
 
 # edit image tags
-if [ ! -z "$IMAGES" ]; then
-  if [ -z "$IMAGE_BASE" ]; then
-    echo "ERROR: define the 'imageBase' when using 'images'!"
+pushd $kustom_folder
+  if [ ! -z "$IMAGES" ]; then
+    if [ -z "$IMAGE_BASE" ]; then
+      echo "ERROR: define the 'imageBase' when using 'images'!"
+      exit 1
+    fi
+    echo "editing $IMAGES with base $IMAGE_BASE"
+    edit_all_images $IMAGE_BASE $IMAGES $VERSION
+  elif [ ! -z "$IMAGE" ]; then
+    edit_image_tag $IMAGE $VERSION
+  else
+    echo "ERROR: one of 'image' or 'images' must be defined"
     exit 1
   fi
-  echo "editing $IMAGES with base $IMAGE_BASE"
-  edit_all_images $IMAGE_BASE $IMAGES $VERSION
-elif [ ! -z "$IMAGE" ]; then
-  edit_image_tag $IMAGE $VERSION
-else
-  echo "ERROR: one of 'image' or 'images' must be defined"
-  exit 1
-fi
-test $? -eq 0 || exit 1
+  test $? -eq 0 || exit 1
+popd
 
-# sync all overlays to catalog app
-if [[ -d "$GITHUB_WORKSPACE/install" ]]; then
-  echo "syncing from apps install folder"
-  rsync -av $GITHUB_WORKSPACE/install/base $REPO_PATH/
-  rsync -av $GITHUB_WORKSPACE/install/overlays/$OVERLAY $REPO_PATH/overlays/ 2>/dev/null
-fi
-
-# commit and push catalog
+# clone and configure the catalog Git repository
 pushd $action_root/catalog
-git add $REPO_PATH
-git commit -F- <<EOF
+  git clone "https://brudicloud:${TOKEN}@${REPO}" catalog
+  git checkout ${REF}
+  git config --local user.email cloud@brudi.com
+  git config --local user.name Mesh
+
+  # configure path to app configuration
+  if [ -z "$REPO_PATH" ]; then
+    # mesh convention: expect apps in 'apps' folder
+    REPO_PATH="apps/${APP}"
+
+    if [[ ! -d "$REPO_PATH" ]] && [ "$is_fallback_app_name" = true ]; then # when using fallback app name but the 'apps' folder
+      # does not exist, we fallback to the 'base' directory.
+      #
+      # The app is now only used in the commit message.
+      REPO_PATH='base'
+    fi
+  fi
+
+  echo "release Mesh app '$APP' ($VERSION) in $REPO"
+  echo "-> $commit_msg"
+  # create app config directory if it doesn't exist yet
+  if [[ ! -d "$REPO_PATH" ]]; then
+    mkdir -p $REPO_PATH
+  fi
+
+
+  # sync all overlays to catalog app
+  if [[ -d "$GITHUB_WORKSPACE/install" ]]; then
+    echo "syncing from apps install folder"
+    rsync -av $GITHUB_WORKSPACE/install/base $REPO_PATH/
+    rsync -av $GITHUB_WORKSPACE/install/overlays/$OVERLAY $REPO_PATH/overlays/ 2>/dev/null
+  fi
+
+  # commit changes to catalog app
+  git add $REPO_PATH
+  git commit -F- <<EOF
 release($APP): upgrade to $VERSION on $REF
 
 $commit_msg
 EOF
-git push origin ${REF}
+
+  # push catalog
+  git push origin ${REF}
 popd
 
+# commit and push workspace changes
 if [ "$PUSH" = true ]; then
-  # commit and push workspace
+  
   pushd $install_folder
-  git add .
-  if [ "$AMEND" = true ]; then
-    git commit --amend --no-edit --no-verify
-  else
-    git commit -F- <<EOF
+    git config --local user.email cloud@brudi.com
+    git config --local user.name Mesh
+
+    git add .
+    
+    if [ "$AMEND" = true ]; then
+      git commit --amend --no-edit --no-verify
+    else
+      git commit -F- <<EOF
 chore($APP): release $VERSION
 
 $commit_msg
 EOF
-  fi
+    fi
   
-  # push the workspace repo itself
-  git push origin HEAD
+    # push the workspace repo itself
+    git push origin HEAD
 
   popd
 
